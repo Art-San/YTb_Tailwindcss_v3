ecf78c5c7c4d289db43e961ba9dfc6aba774a760
Aleksander <asava@sibmail.com>
2023-03-08 19:46:18 +0700

    пауза

--- Доп/4. верстка.jsx
+++ Доп/4. верстка.jsx
@@ -9,6 +9,11 @@
     <li><a href="#">Blog</a></li>
 </ul>
 
+g
+
+//  section#about
+<section id="about"></section>
+
 //p-1 ; pt-1 ; pb-1 ; py-1 (верх-низ) ; px-1 (лево право)
 Есть промежуточные значения в виде pt-0.25 p-2.5 mb-3.5 
 
@@ -71,49 +76,6 @@ lg:text-base (lg=992px шрифт меняется с 20 на 16)
 burger - для этого класа стили в /input.css
 hidden - скрыто  изначально 
 md:block - показывается при md=768px
-
-
-{/* <main>
-    <section id="hero">
-        <div class="container">
-            <h1 class="pt-[100px] font-black text-[50px] max-w-2xl text-center mx-auto 
-            leading-snug lg:text-4xl lg:pt-16 md:text-3xl md:pt-12 md:w-full 
-            sm:pt-10 sm:text-2xl">
-            Grow your business online.
-                Get. More. Leads.</h1>
-
-        <p class="text-xl tracking-tight text-center max-w-xl mx-auto mt-[18px] 
-        md:text-lg md:mt-4 sm:text-base sm:mt-2">Wanna get serious into digital 
-        marketing? Then you need leads and this is where we can help you.</p>
-        </div>
-    </section>
-</main> */}
-
-<!-- pt-[100px] - падинг  с верху -->
-<!-- max-w-2xl - максимально по ширине -->
-<!-- text-center - сам текст отценторовался -->
-<!-- mx-auto - текст встал по центру контейнера (страницы) -->
-<!-- leading-snug - (line-height похоже это межстрочный интервал) snug = 1.375 tight = 1.25 итд -->
-
-<!-- lg:pt-16 - на экране шириной меньше чем 992px падинг топ 16*4=64рх -->
-<!-- lg:text-4xl - текст стал меньше 36рх -->
-
-<!-- md:text-3xl - на экране шириной меньше чем 768рх текст стал 30рх -->
-<!-- md:pt-12 - падинг топ 48зч -->
-<!-- md:max-w-full - ширина 100% -->
-
-<!-- sm:pt-10 - - на экране шириной меньше чем 488рх падинг топ 10*4=40рх
-sm:text-2xl -- на экране шириной меньше чем 488рх текст стал 24рх -->
-
-<!-- text-xl font-size: 1.25rem/* 20px */;-->
-<!-- tracking-tight - межбуквенное растояние -0.025em -->
-<!-- max-w-xl - максимальна ширина подзаголоака xl=36rem/* 576px */ -->
-<!-- mx-auto - текст встал по центру контейнера (страницы) -->
-<!-- mt-[18px] - соответственно -->
-<!-- md:text-lg - на экране шириной меньше чем 768рх текст стал 30рх 1.125rem/* 18px */ -->
-<!-- md:mt-4- на экране шириной меньше чем 768рх margin-top: 1rem/* 16px */
-sm:text-base -  на экране шириной меньше чем 488рх font-size: 1rem/* 16px */;
-sm:mt-2 -  на экране шириной меньше чем 488рх margin-top: 0.5rem/* 8px */ -->
 
 
 
---
+++ Доп/5. верстка певый экран.jsx
@@ -0,0 +1,74 @@
+{/* <main>
+    <section id="hero">
+        <div class="container">
+            <h1 class="pt-[100px] font-black text-[50px] max-w-2xl text-center mx-auto 
+            leading-snug lg:text-4xl lg:pt-16 md:text-3xl md:pt-12 md:w-full 
+            sm:pt-10 sm:text-2xl">
+            Grow your business online.
+                Get. More. Leads.</h1>
+
+            <p class="text-xl tracking-tight text-center max-w-xl mx-auto mt-[18px] 
+            md:text-lg md:mt-4 sm:text-base sm:mt-2">Wanna get serious into digital 
+            marketing? Then you need leads and this is where we can help you.</p>
+
+            <div class="flex justify-center gap-7 sm:flex-col sm:gap-0 sm:mt-4">
+                <a href="" class="inline-block mt-6 py-[10px] px-8 shadow-customred 
+                bg-linghtRed rounded-2xl text-white text-xl tracking-tight sm:text-center 
+                sm:mt-4 sm:text-base">Start here.</a>
+                <a href="" class="inline-block mt-6 py-[10px] px-8 rounded-2xl text-linghtRed 
+                text-xl tracking-tight border-2 border-linghtRed sm:text-center sm:mt-4 
+                sm:text-base">Learn More</a>
+            </div>
+        </div>
+    </section>
+</main> */}
+
+<!-- pt-[100px] - падинг  с верху -->
+<!-- max-w-2xl - максимально по ширине -->
+<!-- text-center - сам текст отценторовался -->
+<!-- mx-auto - текст встал по центру контейнера (страницы) -->
+<!-- leading-snug - (line-height похоже это межстрочный интервал) snug = 1.375 tight = 1.25 итд -->
+
+<!-- lg:pt-16 - на экране шириной меньше чем 992px падинг топ 16*4=64рх -->
+<!-- lg:text-4xl - текст стал меньше 36рх -->
+
+<!-- md:text-3xl - на экране шириной меньше чем 768рх текст стал 30рх -->
+<!-- md:pt-12 - падинг топ 48зч -->
+<!-- md:max-w-full - ширина 100% -->
+
+<!-- sm:pt-10 - - на экране шириной меньше чем 488рх падинг топ 10*4=40рх
+sm:text-2xl -- на экране шириной меньше чем 488рх текст стал 24рх -->
+
+<!-- text-xl font-size: 1.25rem/* 20px */;-->
+<!-- tracking-tight - межбуквенное растояние -0.025em -->
+<!-- max-w-xl - максимальна ширина подзаголоака xl=36rem/* 576px */ -->
+<!-- mx-auto - текст встал по центру контейнера (страницы) -->
+<!-- mt-[18px] - соответственно -->
+<!-- md:text-lg - на экране шириной меньше чем 768рх текст стал 30рх 1.125rem/* 18px */ -->
+<!-- md:mt-4- на экране шириной меньше чем 768рх margin-top: 1rem/* 16px */
+sm:text-base -  на экране шириной меньше чем 488рх font-size: 1rem/* 16px */;
+sm:mt-2 -  на экране шириной меньше чем 488рх margin-top: 0.5rem/* 8px */ -->
+
+<!-- flex - без флекса не сработал justify-center -->
+<!-- justify-center - теги А встали по центру -->
+<!-- gap-7 - добавил растояние между Ашками gap: 1.75rem/* 28px */ -->
+<!-- sm:flex-col - на экране шириной меньше чем 488рх кнопки встают в колонку -->
+<!-- sm:gap-0 - на экране шириной меньше чем 488рх между кнопками уменьшилось растояние  -->
+<!-- sm:mt-4 - margin-top: 1rem/* 16px -->
+
+
+<!-- inline-block -->
+<!-- mt-6- margin-top рх-24 -->
+<!-- py-[10px] - падинг по оси у = 10рх -->
+<!-- px-8 - падинг по оси х = 32рх -->
+<!-- shadow-customred - берем из tailwind.config.js -->
+<!-- bg-linghtRed - цвет берем изtailwind.config.js -->
+<!-- rounded-2xl - скругление улов 16рх -->
+<!-- text-white - белый  -->
+<!-- text-xl - font-size: 1.25rem/* 20px */; -->
+<!-- tracking-tight - растояние межбуквенное letter-spacing: -0.025em; -->
+<!-- border-2 - border-width: 2px -->
+<!-- border-linghtRed - так бордеру добавили цвет -->
+<!-- sm:text-center - соответственно -->
+<!-- sm:mt-4 - margin-top: 1rem/* 16px */ -->
+<!-- sm:text-base - font-size: 1rem/* 16px */; -->
